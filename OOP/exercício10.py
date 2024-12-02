4eed8b2edc3958e06f487f3b3a0ce3fc4b47219a
Gabrieow <gabrielhvazsantos@gmail.com>
2024-12-02 13:06:31 -0300

    Update exercício10.py

--- OOP/exercício10.py
+++ OOP/exercício10.py
@@ -8,16 +8,24 @@ A classe Reserva deve ter o método:
 calcular_valor(): calcula o valor total da reserva (dependendo do número de diárias, por exemplo).
 '''
 
+from datetime import datetime
+
 class Reserva:
-    def __init__(self, cliente, data_entrada, data_saida, valor_total):
+    def __init__(self, cliente, data_entrada, data_saida, valor_diaria):
         self.cliente = cliente
-        self.data_entrada = data_entrada
-        self.data_saida = data_saida
-        self.valor_total = valor_total
+        self.data_entrada = datetime.strptime(data_entrada, "%d/%m/%Y")
+        self.data_saida = datetime.strptime(data_saida, "%d/%m/%Y")
+        self.valor_diaria = valor_diaria
+        self.valor_total = 0
     
-    def calcular_valor():
-        
+    def calcular_valor(self):
+        dias = (self.data_saida - self.data_entrada).days
+        if dias > 0:
+            self.valor_total = dias * self.valor_diaria
+        else:
+            raise ValueError("A data de saída não pode ser antes da data de entrada.")
+        return self.valor_total
 
-cliente1 = Reserva("Gabriel", "01/12/2024", "02/12/2024", 100)
-
-
+reserva1 = Reserva("Gabriel Henrique", "09/12/2024", "16/12/2024", 200)
+valor = reserva1.cacular_valor()
+print(f"Reserva de {reserva.cliente}:\nValor Total: R${valor:.2f}")
